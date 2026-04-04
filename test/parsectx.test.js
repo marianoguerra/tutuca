64d38e23b53c5f4703ffeb8a9258ea5e7db049b5
Mariano Guerra <mariano@marianoguerra.org>
2026-04-04 12:46:21 +0200

    use same window/DOMParser instances in all tests to avoid errors

--- test/parsectx.test.js
+++ test/parsectx.test.js
@@ -1,10 +1,7 @@
 import { expect, test } from "bun:test";
-import { JSDOM } from "jsdom";
 import { ANode } from "../src/anode.js";
+import { DOMParser, Text, Comment } from "./dom.js";
 import { ParseCtxClassSetCollector } from "../src/util/parsectx.js";
-
-const { window } = new JSDOM("");
-const { DOMParser, Text, Comment } = window;
 
 function parse(html) {
   const ctx = new ParseCtxClassSetCollector(DOMParser, Text, Comment);
